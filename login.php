33d7f689b1b57964a6042e4b247498e6ad888c81
Pierre <pierreandregautier974@gmail.com>
2019-12-16 15:12:47 +0100

    login page

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
 <?php require "head.php"; ?>
+
+<body>
+    <div class="form-container">
+        <h1>TINCAT</h1>
+        <form action="functions/setUser.php" method="post">
+            <input type="text" placeholder="pseudo" name="pseudo">
+            <input type="password" placeholder="password" name="password">
+            <input type="submit" value="login">
+        </form>
+    </div>
+
+    <div class="message">
+        <?php 
+            if(isset($_GET["message"])){
+                echo $_GET["message"];
+            }
+        ?>
+    </div>
+
+</body>
+</html>
